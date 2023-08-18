c0ac5c480e39a0081674f18c4a596f549e456477
CodeSandbox <support@codesandbox.io>
2023-08-18 21:51:35 +0000

    Sandbox snapshot commit

--- view/src/App.js
+++ view/src/App.js
@@ -4,13 +4,23 @@ import "./App.css";
 
 function App() {
   const [state, setState] = React.useState(0);
+  const [user, setUser] = React.useState(null);
   const ckickHandle = (props) => {
     setState(state + 1);
+  };
+  const login = () => {
+    fetch("/login")
+      .then((r) => r.json())
+      .then((r) => {
+        setUser(r);
+      });
   };
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={ckickHandle}>Click {state}</button>
+        <button onClick={login}>Login</button>
+        <div>{user && <p>{user.token}</p>}</div>
       </header>
     </div>
   );
